cedbca6dba6669159e51943b57a0046ac084832e
Serhiy Zahoriya <serhiy.int@gmail.com>
2015-09-08 12:11:18 +0300

    Python 3 compatibility, pep8

--- share/templatetags/share_tags.py
+++ share/templatetags/share_tags.py
@@ -1,4 +1,7 @@
-import urlparse
+try:
+    import urlparse
+except:
+    from urllib import parse as urlparse
 from django.template import Library, TemplateSyntaxError, Node
 from django.utils.http import urlquote
 from django.conf import settings
@@ -7,28 +10,41 @@ from django.template.loader import render_to_string
 
 register = Library()
 
+
 @register.simple_tag
 def share_css():
-	return "<link href='" + settings.STATIC_URL +"css/share.css' type='text/css' rel='stylesheet' />"
+    return "<link href='" + settings.STATIC_URL \
+        + "css/share.css' type='text/css' rel='stylesheet' />"
+
 
 @register.simple_tag
 def share_js():
-	return "<script src='" + settings.STATIC_URL +"js/share.js' type='text/javascript'></script>"
+    return "<script src='" + settings.STATIC_URL \
+        + "js/share.js' type='text/javascript'></script>"
+
 
 class ShareNode(Node):
-  def __init__(self, providers=None):
-    self.providers = providers
-  def render(self, context):
-    return render_to_string('share/links.html', {'providers': self.providers, 'url': context['request'].build_absolute_uri()}, context_instance=context)
+    def __init__(self, providers=None):
+        self.providers = providers
+
+    def render(self, context):
+        return render_to_string(
+            'share/links.html',
+            {
+                'providers': self.providers,
+                'url': context['request'].build_absolute_uri()
+            },
+            context_instance=context)
+
 
 @register.tag
 def share(parser, token):
-  args = token.split_contents()
+    args = token.split_contents()
 
-  if len(args) == 1:
-    providers = SHARE_PROVIDERS
-  else:
-    args.pop(0)
-    providers = args
+    if len(args) == 1:
+        providers = SHARE_PROVIDERS
+    else:
+        args.pop(0)
+        providers = args
 
-  return ShareNode(providers)
+    return ShareNode(providers)
